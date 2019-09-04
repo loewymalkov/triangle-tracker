1bcdd0e886531084d955e9dec8458c9e27bffd03
kira roberts and Loewy Malkovich <loewy@>
2019-09-04 15:31:18 -0700

    add front end, functioning jasmine/karma test

--- spec/triangle-spec.js
+++ spec/triangle-spec.js
@@ -1,14 +1,5 @@
 import { Triangle } from './../src/triangle.js';
 describe('Triangle', function() {
-  var reusableTriangle;
-
-  beforeEach(function() {
-    reusableTriangle = new Triangle(5,5,5);
-  });
-
-  it('should show how beforeEach() works', function() {
-    console.log(reusableTriangle);
-  });
 
   it('should test whether a Triangle has three sides', function() {
     var triangle = new Triangle(3,4,5);
@@ -17,9 +8,25 @@ describe('Triangle', function() {
     expect(triangle.side3).not.toEqual(6);
   });
 
-  it('should correctly determine whether three lengths can be made into a triangle', function() {
-    var notTriangle = new Triangle(3,9,22);
-    expect(notTriangle.checkType()).toEqual("not a triangle");
+
+  it('should correctly determine whether three sides are not a triangle', function() {
+    var triangle = new Triangle(12,3,3);
+    console.log(triangle);
+    expect(triangle.checkType()).toEqual("not a triangle");
   });
 
+  it('should determine whether 3 sides of the triangle are equal', function() {
+    var triangle = new Triangle(3,3,3);
+    expect(triangle.checkType()).toEqual("it is an equilateral triangle");
+  });
+
+  it('should determine whether 2 sides of the triangle are equal', function() {
+    var triangle = new Triangle(3,3,2);
+    expect(triangle.checkType()).toEqual("it is an isosceles triangle");
+  });
+
+  it('should determine whether no sides of the triangle are equal', function() {
+    var triangle = new Triangle(3,4,2);
+    expect(triangle.checkType()).toEqual("it is a scalene triangle");
+  });
 });
--- src/main.js
+++ src/main.js
@@ -1,9 +1,18 @@
-// import {  } from './triangle-tracker'; //CHANGE TO CURRENT PROJECT/FILE NAMES
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+import { Triangle } from './triangle.js';
 
 $(document).ready(function() {
-
+  $(".input-form").submit(function(event) {
+    event.preventDefault();
+    let side1 = parseInt($("#input-side1").val());
+    let side2 = parseInt($("#input-side2").val());
+    let side3 = parseInt($("#input-side3").val()) ;
+    let triangle = new Triangle(side1, side2, side3);
+    let result = triangle.checkType(side1, side2, side3)
+    console.log(result);
+    $("#display-output").text(result);
+  })
 });
